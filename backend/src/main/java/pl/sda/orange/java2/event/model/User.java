b598937c64c2634d398d69d61b7b2925490fd9e6
Julia Be <juliabaraniecka@gmail.com>
2023-05-18 20:09:04 +0200

    create user model, event model, role model

---
+++ backend/src/main/java/pl/sda/orange/java2/event/model/User.java
@@ -0,0 +1,22 @@
+package pl.sda.orange.java2.event.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String username;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}

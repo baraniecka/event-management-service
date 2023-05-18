594d2d910e2fc4c5b2095b86008c6e01cd0c2fa7
Julia Be <juliabaraniecka@gmail.com>
2023-05-18 20:15:16 +0200

    add private final field repository

--- backend/src/main/java/pl/sda/orange/java2/event/repository/UserRepository.java
+++ backend/src/main/java/pl/sda/orange/java2/event/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package pl.sda.orange.java2.event.repository;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class UserRepository {
+
+    private final IUserRepository repository;
 }

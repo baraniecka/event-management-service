fb84ce5e1f80379d206033bab76e599590aaa3c0
Julia Be <juliabaraniecka@gmail.com>
2023-05-23 12:41:02 +0200

    add table name to User entity and clean up on UserRepository (unnecessary method removed)

--- backend/src/main/java/pl/sda/orange/java2/event/repository/UserRepository.java
+++ backend/src/main/java/pl/sda/orange/java2/event/repository/UserRepository.java
@@ -2,17 +2,12 @@ package pl.sda.orange.java2.event.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import pl.sda.orange.java2.event.model.Event;
-
-import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepository {
 
-    private final IEventRepository repository;
+    private final IUserRepository repository;
 
-    public List<Event> getAllEvents(){
-        return repository.findAll();
-    }
 }
+

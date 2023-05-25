86c5ef6197ee5240e6aa0d6011992d882ac4dcb6
Julia Be <juliabaraniecka@gmail.com>
2023-05-25 10:28:26 +0200

    frontend - imports update

--- frontend/Event-Managment-Service-Frontend/src/app/app.module.ts
+++ frontend/Event-Managment-Service-Frontend/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EventsComponent
-  ],
+  declarations: [AppComponent, EventsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    // RouterModule.forRoot([{ path: 'events', component: EventsComponent }]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

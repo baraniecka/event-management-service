f8f04ad8638fb68da9a427d729529b94dfade66e
Kinga59 <krozmuszynska@gmail.com>
2023-06-17 21:05:19 +0200

    add components

--- frontend/Event-Managment-Service-Frontend/src/app/app-routing.module.ts
+++ frontend/Event-Managment-Service-Frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SingleEventComponent } from './single-event/single-event.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
+import { ViewForLoggedUserComponent} from './view-for-logged-user/view-for-logged-user.component'
 
 const routes: Routes = [
   { path: 'events', component: EventsComponent },
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'event/show/:id', component: SingleEventComponent },
   { path: 'event/add', component: CreateEventComponent },
   {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'events/logged', component: ViewForLoggedUserComponent}
 ];
 
 @NgModule({
--- frontend/Event-Managment-Service-Frontend/src/app/view-for-logged-user/view-for-logged-user.component.ts
+++ frontend/Event-Managment-Service-Frontend/src/app/view-for-logged-user/view-for-logged-user.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { EventsService } from '../service/events.service';
+import { Event } from '../model/event';
 @Component({
   selector: 'app-view-for-logged-user',
   templateUrl: './view-for-logged-user.component.html',
   styleUrls: ['./view-for-logged-user.component.scss']
 })
 export class ViewForLoggedUserComponent {
+  events: Event[];
+  singleEvent: Event;
 
+  constructor(private eventService: EventsService) {}
+
+  findActualEvents(): void {
+    this.eventService
+      .getActualEvents()
+      .subscribe((events) => (this.events = events));
+  }
+
+  ngOnInit(): void {
+    this.findActualEvents();
+  }
 }
